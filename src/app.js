8963ab348f303de5355cb14572fb30f8606ae6a9
rubyheald <ruby.heald@barclays.com>
2024-05-20 12:30:51 +0100

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,10 @@ const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
 
+app.get("/resturants", async (request, response) =>{
+    const restaurants = await Restaurant.findAll();
+    response.json(restaurants);
 
-
+})
 
 module.exports = app;
