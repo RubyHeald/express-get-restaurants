17991325c06892a122fdecb1f43781a62d33950a
rubyheald <ruby.heald@barclays.com>
2024-05-21 15:42:18 +0100

    express 2 extension complete

--- src/app.js
+++ src/app.js
@@ -3,12 +3,34 @@ const app = express();
 const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
-//TODO: Create your GET Request Route Below: 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
 app.get("/restaurants", async (request, response) =>{
     const restaurant = await Restaurant.findAll();
-    response.json(restaurant);
-
+        response.json(restaurant);
 })
+
+app.get("/restaurants/:id", async (request, response) =>{
+    const number = req.params.id;
+        const restaurant = await Restaurant.findByPk(number);
+            response.json(restaurant)
+})
+
+app.post("resturants", async (req, res) =>{
+    const restaurant = await Restaurant.create(rew, body)
+    res.json(restaurant)
+})
+
+app.put("resturants/:id", async (req, res) =>{
+    const updatedRest = await Restaurant.update(req.body,{where:{id: req.params.id}})
+    res.json(updatedRest)
+})
+
+app.delete("resturants/:id", async (req, res)=>{
+    const deletedRest = await Restaurant.destroy({where: {id: req.params.id}})
+    res.json(deletedRest)
+})
+
 
 module.exports = app;
